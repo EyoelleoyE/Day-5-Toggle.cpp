dbbbf7ad00ec04d33b29093d2a52a7933f32bc0d
Eyoel Hailu <99424828+EyoelleoyE@users.noreply.github.com>
2026-03-18 22:49:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    bool isLighton = false;
+
+    cout<<"Initial State: "<<isLighton<<"(0 means OFF)"<<endl;
+
+    isLighton = !isLighton;
+
+    cout<<"Flipped State: "<<isLighton<<"(1 means ON)"<<endl;
+    cout << "March 18, let's GO!!!";
+    return 0;
+}
